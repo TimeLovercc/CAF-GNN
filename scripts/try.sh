ad0054bd50ebfc441125ead44737b47bda0956e9
“TmeLoverccâ <gzjz07@outlook.com>
2023-10-11 20:15:53 +0000

    change the configs

--- scripts/try.sh
+++ scripts/try.sh
@@ -1,3 +1,3 @@
 
 # try bail GCN
-CUDA_VISIBLE_DEVICES=3 python ./src/main.py --dataset_name german --model_name caf --seed 3 
+CUDA_VISIBLE_DEVICES=3 python ./src/main.py --dataset_name credit --model_name caf --seed 3 
