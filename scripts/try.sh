0a1b157afe18f4f4597dda3a46aac7c8c49d4645
“TmeLoverccâ <gzjz07@outlook.com>
2023-10-12 05:47:15 +0000

    add configs

--- scripts/try.sh
+++ scripts/try.sh
@@ -1,3 +1,3 @@
 
 # try bail GCN
-CUDA_VISIBLE_DEVICES=2 python ./src/main.py --dataset_name bail --model_name caf --seed 4
+CUDA_VISIBLE_DEVICES=2 python ./src/main.py --dataset_name bail --model_name sage --seed 2
