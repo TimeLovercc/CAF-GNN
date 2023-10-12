69668cef11065f9c8af85d5cccdf052cdf37de6c
“TmeLoverccâ <gzjz07@outlook.com>
2023-10-12 06:32:28 +0000

    add description in md

--- README.md
+++ README.md
@@ -1,5 +1,132 @@
-# Towards Fair Graph Neural Networks via Graph Counterfactual
-An official PyTorch implementation of "Towards Fair Graph Neural Networks via Graph Counterfactual" (CIKM 2023). If you find this repo to be useful, please cite our paper. Thank you.
+# CAF-GNN 
+This repository is an official PyTorch implementation of ["Towards Fair Graph Neural Networks via Graph Counterfactual"](https://arxiv.org/abs/2307.04937) (CIKM 2023). 
 
-## Notes
-The code has not been organized yet. We will organize it as soon as possible. If you have any questions, please use github issues.
+## Recent Updates
+We're excited to announce that our repository has recently transitioned to using PyTorch Lightning! This integration not only simplifies the codebase and makes our repository more maintainable but also offers numerous benefits like:
+- **Scalability**: Effortlessly scale your models to run on more GPUs, TPUs, or even across multiple machines without changing your code.
+- **Flexibility**: PyTorch Lightning's modular design makes your code more organized, easier to understand, and allows you to swap different components without hassle.
+- **Reproducibility**: Ensures your experiments are reproducible, and you can keep track of all parameters, metrics, and artifacts with minimal code.
+### ⚠️ Warning
+While we are thrilled about the transition to PyTorch Lightning, it’s important to note that the transfer from the original codebase is not thoroughly verified and may contain small mistakes or inconsistencies. We appreciate your understanding and encourage users to raise any issues or discrepancies found, ensuring continual improvement and refinement of the codebase. Feel free to open an issue for any problems or suggestions you might have!
+
+![Figure](https://github.com/TimeLovercc/CAF-GNN/blob/main/assets/visual.pdf)
+
+## Installation
+To set up the necessary environment, follow the steps below:
+
+1. **Clone the repository:**
+   ```
+   git clone git@github.com:TimeLovercc/CAF-GNN.git
+   cd CAF-GNN
+   ```
+
+2. **Project Setup:**
+   Setting up this project involves a few comprehensive steps. Here's how to get everything ready:
+
+   - **Review and Run Setup Script:**
+     It's important to first review the `scripts/setup.sh` script as it includes several setup procedures, some of which might not be necessary for every user, such as the installation of Anaconda. Open the script in your preferred text editor, and feel free to comment out or modify any sections not applicable to your setup. After tailoring the script to your needs, execute it by running:
+     ```
+     bash scripts/setup.sh
+     ```
+     This command will take care of installing all the necessary packages and libraries required for the project, according to your adjustments.
+
+   - **Install PyTorch Geometric:**
+     Following the base setup, you'll need to install PyTorch Geometric. Detailed instructions for this process are provided [here](https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html).
+
+   - **Set up Weights & Biases (wandb):**
+     Our project utilizes Weights & Biases for efficient experiment tracking, insightful visualization, and convenient data management. Set it up with:
+     ```
+     pip install wandb
+     ```
+     If you're not already using wandb, you'll need to create an account. You can log in via the command line using:
+     ```
+     wandb login
+     ```
+
+   These steps will ensure your environment is fully configured and ready for running experiments with our project.
+
+
+## Usage
+After completing the installation steps, you're set to start experimenting with the project. You can initiate the process using the following command:
+```
+bash scripts/try.sh
+```
+
+This script runs the main application with default or pre-specified parameters. However, if you want to customize the execution, you can directly use the command line as follows:
+```
+CUDA_VISIBLE_DEVICES=[GPU_ID] python ./src/main.py --dataset_name [DATASET] --model_name [MODEL] --seed [SEED] [--no_train]
+```
+- Replace `[GPU_ID]` with the ID of the GPU you want to use.
+- `[DATASET]` can be one of the following: `german`, `credit`, `bail`.
+- `[MODEL]` can be one of the following: `gcn`, `sage`, `caf`.
+- `[SEED]` is an integer for the random seed.
+- Adding `--no_train` is optional and it tells the system to skip the training phase and instead load the latest trained base model.
+
+For instance, to run the application on the 'bail' dataset using the 'sage' model with a specific seed, you would use:
+```
+CUDA_VISIBLE_DEVICES=2 python ./src/main.py --dataset_name german --model_name sage --seed 2
+```
+
+Please ensure you navigate to the project's root directory before initiating the script or command line execution.
+
+For more detailed information or custom configurations, you might want to refer to additional documentation or explore the `./src/main.py` script to understand all available options and how they impact the model's behavior and performance.
+
+## Explore with Jupyter Notebooks
+
+Dive into interactive experimentation and exploration with our Jupyter Notebooks in the `notebooks/example.ipynb`. This illustrative guide facilitates a hands-on approach to data visualization, model training, and analysis within our project's ecosystem. Navigate through each section, from setting up and exploring data to model training and analysis, all while enjoying the flexibility to modify parameters and configurations for personalized experiments. Your journey through model interactions and data insights is enhanced with the capability to run cells, observe real-time results, and even create your own experiment scenarios. Whether you're analyzing, sharing findings, or contributing your own experiments and visualizations, our notebooks provide a comprehensive and interactive platform to enhance your experience with our project. We use `german` dataset and `caf` method as example.
+
+## Data Acquisition
+This project relies on specific datasets that need to be prepared before running the models. The datasets can be obtained from an external repository and set up as follows:
+
+1. **Download Data:**
+   The datasets are available on [this GitHub repository](https://github.com/chirag126/nifty). Navigate to the repository and download the necessary files. You will need all the `.csv` and `.txt` files for each dataset.
+
+2. **Prepare Data Directory:**
+   Within the `CAF-GNN` project, create a directory structure to store the data. If it doesn't already exist, create a `data` folder at the root, and within it, create a subfolder for each dataset you're using (e.g., `data/dataset_name`). Each of these dataset folders should have a `raw` subfolder.
+
+3. **Place Data Files:**
+   Copy all the `.csv` and `.txt` files you downloaded for each dataset into the corresponding `data/dataset_name/raw` folder in your local `CAF-GNN` project.
+
+Here's a command-line sequence to make the process clearer:
+
+```sh
+# Navigate to your CAF-GNN directory (replace with your actual directory path)
+cd path/to/CAF-GNN
+
+# Create a data directory and a subdirectory for your dataset (replace 'dataset_name' with your actual dataset's name)
+mkdir -p data/dataset_name/raw
+
+# Now, manually copy the dataset files into the newly created 'raw' folder
+```
+
+Please repeat the process for each dataset you plan to use. Ensure the files are correctly placed so the project's scripts can access and process them.
+
+After setting up the data, proceed with the usage instructions as described in the Usage section.
+
+
+## Contributing
+We welcome contributions that improve the code, documentation, or other aspects of the project. If you're interested in contributing, please start by discussing the change you wish to make via an issue. Afterward, you can make your changes and create a pull request.
+
+Please ensure to update tests as appropriate and maintain the quality of the codebase.
+
+## License
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+Zhimeng Guo - gzjz07 [at] outlook.com
+
+Project Link: [https://github.com/TimeLovercc/CAF-GNN](https://github.com/TimeLovercc/CAF-GNN)
+
+## Citation
+If you find our research useful, please consider citing our work:
+
+```bibtex
+@article{guo2023towards,
+  title={Towards Fair Graph Neural Networks via Graph Counterfactual},
+  author={Guo, Zhimeng and Li, Jialiang and Xiao, Teng and Ma, Yao and Wang, Suhang},
+  journal={arXiv preprint arXiv:2307.04937},
+  year={2023}
+}
+```
+
+Thank you for your interest in our project! We hope you find this research useful and look forward to seeing your contributions and discussions.
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,17 @@
 import re
 from pathlib import Path
 from datetime import datetime
+import torch
+import numpy as np
+from scipy import sparse
+from sklearn.preprocessing import normalize
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics.pairwise import cosine_similarity
+import scipy.io as scio
+
+# set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def find_latest_best_checkpoint(base_dir):
     # Recursively find all 'lightning_logs' directories
@@ -40,4 +51,237 @@ def find_latest_best_checkpoint(base_dir):
     else:
         print(f"No best checkpoint found in the latest run at: {latest_run}")
         return None
+    
 
+
+
+
+
+def pre_analysis(adj, labels, sens):
+    '''
+    :param
+        labels: n
+        sens: n
+        adj: csr_sparse, n x n
+    :return: this function analyze:
+        1. the correlation between the sensitive attributes of neighbors and the labels
+        2. the correlation between the sensitive attributes of itself and the labels
+        S_N(i), Y_i | S_i  not independent ->
+    '''
+    adj_noself = adj.copy()
+    adj_noself.setdiag(0)  # remove edge v -> v
+    if (adj_noself != adj_noself.T).nnz == 0:
+        print("symmetric!")
+    else:
+        print("not symmetric!")
+    adj_degree = adj.sum(axis=1)
+    ave_degree = adj_degree.sum()/len(adj_degree)
+    print('averaged degree: ', ave_degree, ' max degree: ', adj_degree.max(), ' min degree: ', adj_degree.min())
+
+    # inter- and intra- connections
+    node_num = adj.shape[0]
+    edge_num = (len(adj.nonzero()[0]) - node_num) / 2
+    intra_num = 0
+    for u, v in zip(*adj.nonzero()):  # u -> v
+        if u >= v:
+            continue
+        if sens[u] == sens[v]:
+            intra_num += 1
+    print("edge num: ", edge_num, " intra-group edge: ", intra_num, " inter-group edge: ", edge_num - intra_num)
+
+    # row-normalize
+    adj_noself = normalize(adj_noself, norm='l1', axis=1)
+    nb_sens_ave = adj_noself @ sens  # n x 1, average sens of 1-hop neighbors
+
+    # Y_i, S_i
+    #pyplot.scatter(labels, sens)
+    #pyplot.show()
+
+    cov_results = stats_cov(labels, sens)
+    print('correlation between Y and S:', cov_results)
+
+    # S_N(i), Y_i | S_i
+    cov_nb_results = stats_cov(labels, nb_sens_ave)
+    print('correlation between Y and neighbors (not include self)\' S:', cov_nb_results)
+
+    # R^2
+    X = sens.reshape(node_num, -1)
+    reg = LinearRegression().fit(X, labels)
+    y_pred = reg.predict(X)
+    R2 = r2_score(labels, y_pred)
+    print('R2 - self: ', R2, ' ', reg.score(X, labels))
+
+    X = nb_sens_ave.reshape(node_num, -1)
+    reg = LinearRegression().fit(X, labels)
+    y_pred = reg.predict(X)
+    R2 = r2_score(labels, y_pred)
+    print('R2 - neighbor: ', R2, ' ', reg.score(X, labels))
+
+    return
+
+
+
+def generate_synthetic_data_old(path):
+
+    sens = np.random.binomial(n=1, p=p, size=n)
+    embedding = np.random.normal(loc=0, scale=1, size=(n, z_dim))
+    feat_idxs = random.sample(range(z_dim), dim)
+    v = np.random.normal(0, 1, size=(1, dim))
+    features = embedding[:, feat_idxs] + (np.dot(sens.reshape(-1,1), v))  # (n x dim) + (1 x dim) -> n x dim
+
+    adj = np.zeros((n, n))
+    sens_sim = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i, n):  # i<=j
+            if i == j:
+                sens_sim[i][j] = 1
+                continue
+            sens_sim[i][j] = sens_sim[j][i] = (sens[i] == sens[j])
+            # sim_ij = 1 - spatial.distance.cosine(embedding[i], embedding[j])  # [-1, 1]
+            # adj[i][j] = adj[j][i] = sim_ij + alpha * (sens[i] == sens[j])
+
+    similarities = cosine_similarity(embedding)  # n x n
+    adj = similarities + alpha * sens_sim
+
+    print('adj max: ', adj.max(), ' min: ', adj.min())
+    adj[np.where(adj >= 0.4)] = 1
+    adj[np.where(adj < 0.4)] = 0
+    adj = sparse.csr_matrix(adj)
+
+    w = np.random.normal(0, 1, size=(z_dim, 1))
+    w_s = 1
+
+    adj_norm = normalize(adj, norm='l1', axis=1)
+    nb_sens_ave = adj_norm @ sens  # n x 1, average sens of 1-hop neighbors
+
+    dd = np.matmul(embedding, w)
+    d2 = nb_sens_ave.reshape(-1,1)
+    print('y component: ', np.mean(dd), np.mean(d2))
+    labels = np.matmul(embedding, w) + w_s * nb_sens_ave.reshape(-1,1) # n x 1
+    labels = labels.reshape(-1)
+    labels_mean = np.mean(labels)
+    labels_binary = np.zeros_like(labels)
+    labels_binary[np.where(labels > labels_mean)] = 1.0
+
+    print('pos labels: ', labels_binary.sum(), ' neg: ', len(labels_binary) - labels_binary.sum())
+
+    # statistics
+    pre_analysis(adj, labels, sens)
+
+    data = {'x': features, 'adj': adj, 'labels': labels_binary, 'sens': sens,
+            'z': embedding, 'v': v, 'feat_idxs': feat_idxs, 'alpha': alpha, 'w': w, 'w_s': w_s}
+    scio.savemat(path, data)
+    print('data saved in ', path)
+    return data
+
+
+def generate_synthetic_data(path, n, z_dim, p, q, alpha, beta, threshold, dim):
+    sens = np.random.binomial(n=1, p=p, size=n)
+    sens_repeat = np.repeat(sens.reshape(-1, 1), z_dim, axis=1)
+    sens_embedding = np.random.normal(loc=sens_repeat, scale=1, size=(n, z_dim))
+    labels = np.random.binomial(n=1, p=q, size=n)
+    labels_repeat = np.repeat(labels.reshape(-1, 1), z_dim, axis=1)
+    labels_embedding = np.random.normal(loc=labels_repeat, scale=1, size=(n, z_dim))
+    features_embedding = np.concatenate((sens_embedding, labels_embedding), axis=1)
+    weight = np.random.normal(loc=0, scale=1, size=(z_dim*2, dim))
+    # features = np.matmul(features_embedding, weight)
+    features = np.matmul(features_embedding, weight) + np.random.normal(loc=0, scale=1, size=(n, dim))
+
+    adj = np.zeros((n, n))
+    sens_sim = np.zeros((n, n))
+    labels_sim = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i, n):  # i<=j
+            if i == j:
+                sens_sim[i][j] = -1
+                labels_sim[i][j] = -1
+                continue
+            sens_sim[i][j] = sens_sim[j][i] = (sens[i] == sens[j])
+            labels_sim[i][j] = labels_sim[j][i] = (labels[i] == labels[j])
+            # sim_ij = 1 - spatial.distance.cosine(embedding[i], embedding[j])  # [-1, 1]
+            # adj[i][j] = adj[j][i] = sim_ij + alpha * (sens[i] == sens[j])
+
+    similarities = cosine_similarity(features_embedding)  # n x n
+    similarities[np.arange(n), np.arange(n)] = -1
+    adj = similarities + alpha * sens_sim + beta * labels_sim
+    print('adj max: ', adj.max(), ' min: ', adj.min())
+    adj[np.where(adj >= threshold)] = 1
+    adj[np.where(adj < threshold)] = 0
+    edge_num = adj.sum()
+    adj = sparse.csr_matrix(adj)
+    # features = np.concatenate((sens.reshape(-1,1), features), axis=1)
+
+    # generate counterfactual
+    sens_flip = 1 - sens
+    sens_flip_repeat = np.repeat(sens_flip.reshape(-1, 1), z_dim, axis=1)
+    sens_flip_embedding = np.random.normal(loc=sens_flip_repeat, scale=1, size=(n, z_dim))
+    features_embedding = np.concatenate((sens_flip_embedding, labels_embedding), axis=1)
+    features_cf = np.matmul(features_embedding, weight) + np.random.normal(loc=0, scale=1, size=(n, dim))
+
+    adj_cf = np.zeros((n, n))
+    sens_cf_sim = np.zeros((n, n))
+    labels_cf_sim = np.zeros((n, n))
+    for i in range(n):
+        for j in range(i, n):
+            if i == j:
+                sens_cf_sim[i][j] = -1
+                labels_cf_sim[i][j] = -1
+                continue
+            sens_cf_sim[i][j] = sens_cf_sim[j][i] = (sens_flip[i] == sens_flip[j])
+            labels_cf_sim[i][j] = labels_cf_sim[j][i] = (labels[i] == labels[j])
+    
+    similarities_cf = cosine_similarity(features_cf)  # n x n
+    similarities_cf[np.arange(n), np.arange(n)] = -1
+    adj_cf = similarities_cf + alpha * sens_cf_sim + beta * labels_cf_sim
+    print('adj_cf max', adj_cf.max(), ' min: ', adj_cf.min())
+    adj_cf[np.where(adj_cf >= threshold)] = 1
+    adj_cf[np.where(adj_cf < threshold)] = 0
+    adj_cf = sparse.csr_matrix(adj_cf)
+    # features_cf = np.concatenate((sens_flip.reshape(-1,1), features_cf), axis=1)
+
+    # statistics
+    # pre_analysis(adj, labels, sens)
+    print('edge num: ', edge_num)
+    data = {'x': features, 'adj': adj, 'labels': labels, 'sens': sens, 'x_cf': features_cf, 'adj_cf': adj_cf}
+    # scio.savemat(path, data)
+    print('data saved in ', path)
+    return data
+
+if __name__ == '__main__':
+    n = 2000
+    z_dim = 50
+    p = 0.4
+    q = 0.3
+    alpha = 0.01  #
+    beta = 0.01
+    threshold = 0.6
+    dim = 32
+
+    dataset = 'synthetic'
+    path_root = './dataset/synthetic/'
+    path = path_root+f'synthetic_{n}_{z_dim}_{p}_{q}_{alpha}_{beta}_{threshold}_{dim}.mat'
+    generate_synthetic_data(path, n, z_dim, p, q, alpha, beta, threshold, dim)
+
+
+
+
+# edge count
+# (pytorch) root@d3d3890e5fe5:~/projects/causal-fairness-public# python synthetic_generation.py 
+# adj max:  0.7485439353207947  min:  -1.02
+# adj_cf max 1.02  min:  -1.02
+# edge num:  417930.0
+# data saved in  ./dataset/synthetic/synthetic_2000_50_0.4_0.3_0.01_0.01_0.4.mat
+# (pytorch) root@d3d3890e5fe5:~/projects/causal-fairness-public# python synthetic_generation.py 
+# adj max:  0.7913202327923252  min:  -1.02
+# adj_cf max 1.02  min:  -1.02
+# edge num:  123642.0
+# data saved in  ./dataset/synthetic/synthetic_2000_50_0.4_0.3_0.01_0.01_0.5.mat
+# (pytorch) root@d3d3890e5fe5:~/projects/causal-fairness-public# python synthetic_generation.py 
+# adj max:  0.7675836890785276  min:  -1.02
+# adj_cf max 1.02  min:  -1.02
+# edge num:  14408.0
+# data saved in  ./dataset/synthetic/synthetic_2000_50_0.4_0.3_0.01_0.01_0.6.mat
+# (pytorch) root@d3d3890e5fe5:~/projects/causal-fairness-public# python synthetic_generation.py 
+# adj max:  0.7627960018969875  min:  -1.02
+# adj_cf max 1.02  min:  -1.02
+# edge num:  134.0
